5eae70a35b0e8eae4f9772cd56f2e3bc40ebd436
Juma Michael <obudho76@gmail.com>
2025-05-27 21:04:43 +0300

    initiall commit

--- lib/seed.py
+++ lib/seed.py
@@ -32,29 +32,49 @@ bee = Species(name="Bee")
 
 #  Add Symptoms
 
-fever = Symptom(name="Fever", description="Elevated body temperature, often indicating infection or inflammation.")
-cough = Symptom(name="Cough", description="Repetitive hacking sound caused by irritation in the respiratory tract.")
-skin_rash = Symptom(name="Skin Rash", description="Irritated, red, or inflamed skin that may indicate allergies or parasites.")
-blood_in_stool = Symptom(name="Blood in Stool", description="Presence of blood in feces, which can suggest internal bleeding or infections.")
-weight_loss = Symptom(name="Weight Loss", description="Unexplained loss of body mass, often due to illness, parasites, or poor nutrition.")
-lameness = Symptom(name="Lameness", description="Difficulty in walking or limping, usually caused by injury or hoof diseases.")
-loss_of_appetite = Symptom(name="Loss of Appetite", description="Reduced desire to eat, often indicating discomfort or systemic illness.")
-diarrhea = Symptom(name="Diarrhea", description="Frequent, loose or watery bowel movements, often caused by infection or dietary upset.")
-constipation = Symptom(name="Constipation", description="Difficulty passing stool, which may indicate digestive issues.")
-vomiting = Symptom(name="Vomiting", description="Ejection of stomach contents through the mouth due to gastrointestinal distress.")
+fever = Symptom(
+    name="Fever", description="Elevated body temperature, often indicating infection or inflammation.")
+cough = Symptom(
+    name="Cough", description="Repetitive hacking sound caused by irritation in the respiratory tract.")
+skin_rash = Symptom(
+    name="Skin Rash", description="Irritated, red, or inflamed skin that may indicate allergies or parasites.")
+blood_in_stool = Symptom(
+    name="Blood in Stool", description="Presence of blood in feces, which can suggest internal bleeding or infections.")
+weight_loss = Symptom(
+    name="Weight Loss", description="Unexplained loss of body mass, often due to illness, parasites, or poor nutrition.")
+lameness = Symptom(
+    name="Lameness", description="Difficulty in walking or limping, usually caused by injury or hoof diseases.")
+loss_of_appetite = Symptom(
+    name="Loss of Appetite", description="Reduced desire to eat, often indicating discomfort or systemic illness.")
+diarrhea = Symptom(
+    name="Diarrhea", description="Frequent, loose or watery bowel movements, often caused by infection or dietary upset.")
+constipation = Symptom(
+    name="Constipation", description="Difficulty passing stool, which may indicate digestive issues.")
+vomiting = Symptom(
+    name="Vomiting", description="Ejection of stomach contents through the mouth due to gastrointestinal distress.")
 
 # Add Treatments
-antibiotics = Treatment(name="Antibiotics", method="Administer orally or via injection to combat bacterial infections.")
-hydration = Treatment(name="Hydration Therapy", method="Provide clean water and electrolyte solutions to restore fluid balance.")
-deworming = Treatment(name="Deworming", method="Use anthelmintic drugs to eliminate internal parasites.")
-#vaccination = Treatment(name="Vaccination", method="Administer vaccines to prevent viral and bacterial infections.")
-topical_ointment = Treatment(name="Topical Ointment", method="Apply medicated cream to affected skin areas.")
-anti_inflammatory = Treatment(name="Anti-Inflammatory Drugs", method="Inject or administer orally to reduce swelling and pain.")
-pain_relief = Treatment(name="Pain Relief", method="Use analgesics to alleviate pain symptoms.")
-wound_cleaning = Treatment(name="Wound Cleaning", method="Clean with antiseptic solution and dress to prevent infection.")
-antipyretics = Treatment(name="Antipyretics", method="Use fever reducers like paracetamol to lower high body temperature.")
-nutritional_supplements = Treatment(name="Nutritional Supplements", method="Provide minerals and vitamins to boost immune function.")
-isolation = Treatment(name="Isolation", method="Separate the sick animal to prevent disease spread.")
+antibiotics = Treatment(
+    name="Antibiotics", method="Administer orally or via injection to combat bacterial infections.")
+hydration = Treatment(name="Hydration Therapy",
+                      method="Provide clean water and electrolyte solutions to restore fluid balance.")
+deworming = Treatment(
+    name="Deworming", method="Use anthelmintic drugs to eliminate internal parasites.")
+# vaccination = Treatment(name="Vaccination", method="Administer vaccines to prevent viral and bacterial infections.")
+topical_ointment = Treatment(
+    name="Topical Ointment", method="Apply medicated cream to affected skin areas.")
+anti_inflammatory = Treatment(name="Anti-Inflammatory Drugs",
+                              method="Inject or administer orally to reduce swelling and pain.")
+pain_relief = Treatment(
+    name="Pain Relief", method="Use analgesics to alleviate pain symptoms.")
+wound_cleaning = Treatment(
+    name="Wound Cleaning", method="Clean with antiseptic solution and dress to prevent infection.")
+antipyretics = Treatment(
+    name="Antipyretics", method="Use fever reducers like paracetamol to lower high body temperature.")
+nutritional_supplements = Treatment(
+    name="Nutritional Supplements", method="Provide minerals and vitamins to boost immune function.")
+isolation = Treatment(
+    name="Isolation", method="Separate the sick animal to prevent disease spread.")
 
 # Preventive Measures
 vaccination = Prevention(
@@ -179,25 +199,36 @@ rabies = Disease(
 
 # Add relationships
 fmd.species.extend([cow, goat, sheep, pig, horse, llama, alpaca])
-fmd.symptoms.extend([fever, skin_rash, cough, blood_in_stool, weight_loss, lameness, loss_of_appetite, diarrhea, constipation, vomiting])
-fmd.treatments.extend([antibiotics, hydration, deworming, topical_ointment, anti_inflammatory, pain_relief, wound_cleaning, antipyretics, nutritional_supplements, isolation])
-fmd.preventions.extend([vaccination, sanitation, quarantine, biosecurity, proper_nutrition, vector_control, clean_water, regular_health_checks, stress_reduction, proper_waste_management, good_husbandry_practices, prompt_treatment, vaccination_schedule, rodent_control, environmental_control])
+fmd.symptoms.extend([fever, skin_rash, cough, blood_in_stool, weight_loss,
+                    lameness, loss_of_appetite, diarrhea, constipation, vomiting])
+fmd.treatments.extend([antibiotics, hydration, deworming, topical_ointment, anti_inflammatory,
+                      pain_relief, wound_cleaning, antipyretics, nutritional_supplements, isolation])
+fmd.preventions.extend([vaccination, sanitation, quarantine, biosecurity, proper_nutrition, vector_control, clean_water, regular_health_checks,
+                       stress_reduction, proper_waste_management, good_husbandry_practices, prompt_treatment, vaccination_schedule, rodent_control, environmental_control])
 
 anthrax.species.extend([cow, goat, chicken, pig, sheep,
                        duck, horse, rabbit, turkey, alpaca, llama, cat, dog])
-anthrax.symptoms.extend([fever, cough, skin_rash, blood_in_stool, weight_loss, lameness, loss_of_appetite, diarrhea, constipation, vomiting])
-anthrax.treatments.extend([antibiotics, hydration, deworming, topical_ointment, anti_inflammatory, pain_relief, wound_cleaning, antipyretics, nutritional_supplements, isolation])
-anthrax.preventions.extend([vaccination, sanitation, quarantine, biosecurity, proper_nutrition, vector_control, clean_water, regular_health_checks, stress_reduction, proper_waste_management, good_husbandry_practices, prompt_treatment, vaccination_schedule, rodent_control, environmental_control])
+anthrax.symptoms.extend([fever, cough, skin_rash, blood_in_stool, weight_loss,
+                        lameness, loss_of_appetite, diarrhea, constipation, vomiting])
+anthrax.treatments.extend([antibiotics, hydration, deworming, topical_ointment, anti_inflammatory,
+                          pain_relief, wound_cleaning, antipyretics, nutritional_supplements, isolation])
+anthrax.preventions.extend([vaccination, sanitation, quarantine, biosecurity, proper_nutrition, vector_control, clean_water, regular_health_checks,
+                           stress_reduction, proper_waste_management, good_husbandry_practices, prompt_treatment, vaccination_schedule, rodent_control, environmental_control])
 
 swine_erysipelas.species.extend([pig])
-swine_erysipelas.symptoms.extend([fever, skin_rash, cough, blood_in_stool, weight_loss, lameness, loss_of_appetite, diarrhea, constipation, vomiting])
-swine_erysipelas.treatments.extend([antibiotics, hydration, deworming, topical_ointment, anti_inflammatory, pain_relief, wound_cleaning, antipyretics, nutritional_supplements, isolation])
-swine_erysipelas.preventions.extend([vaccination, sanitation, quarantine, biosecurity, proper_nutrition, vector_control, clean_water, regular_health_checks, stress_reduction, proper_waste_management, good_husbandry_practices, prompt_treatment, vaccination_schedule, rodent_control, environmental_control])
+swine_erysipelas.symptoms.extend([fever, skin_rash, cough, blood_in_stool,
+                                 weight_loss, lameness, loss_of_appetite, diarrhea, constipation, vomiting])
+swine_erysipelas.treatments.extend([antibiotics, hydration, deworming, topical_ointment, anti_inflammatory,
+                                   pain_relief, wound_cleaning, antipyretics, nutritional_supplements, isolation])
+swine_erysipelas.preventions.extend([vaccination, sanitation, quarantine, biosecurity, proper_nutrition, vector_control, clean_water, regular_health_checks,
+                                    stress_reduction, proper_waste_management, good_husbandry_practices, prompt_treatment, vaccination_schedule, rodent_control, environmental_control])
 
 rabies.species.extend([dog, cat, cow, goat, sheep, pig, horse])
-rabies.symptoms.extend([fever, cough, skin_rash, blood_in_stool, weight_loss, lameness, loss_of_appetite, diarrhea, constipation, vomiting])
+rabies.symptoms.extend([fever, cough, skin_rash, blood_in_stool, weight_loss,
+                       lameness, loss_of_appetite, diarrhea, constipation, vomiting])
 rabies.treatments.extend([antibiotics, hydration, isolation])
-rabies.preventions.extend([vaccination, sanitation, quarantine, biosecurity, proper_nutrition, vector_control, clean_water, regular_health_checks, stress_reduction, proper_waste_management, good_husbandry_practices, prompt_treatment, vaccination_schedule, rodent_control, environmental_control])
+rabies.preventions.extend([vaccination, sanitation, quarantine, biosecurity, proper_nutrition, vector_control, clean_water, regular_health_checks,
+                          stress_reduction, proper_waste_management, good_husbandry_practices, prompt_treatment, vaccination_schedule, rodent_control, environmental_control])
 
 # Commit All to DB
 session.add_all([cow, goat, chicken, pig, sheep, duck, horse, rabbit, turkey, alpaca, llama, cat,
